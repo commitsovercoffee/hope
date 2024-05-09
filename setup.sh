69551ba32aa8adba2aa523096f70814d892a727f
commitsovercoffee <commitsovercoffee@gmail.com>
2024-05-10 03:50:23 +0530

    refactors

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ multilib () {
     echo "Include = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
 
     # refresh database.
-    pacman -Syy reflector --noconfirm
+    pacman -Syy reflector
     reflector --country India --protocol https --save /etc/pacman.d/mirrorlist
 
 }
@@ -38,9 +38,6 @@ locale () {
     # set system locale ~ creates 'locale.conf'.
     localectl set-locale LANG=en_US.UTF-8
 
-    # install fonts.
-    pacman -S ttf-firacode-nerd nerd-fonts noto-fonts noto-fonts-extra noto-fonts-emoji font-manager --noconfirm
-
 }
 
 network () {
@@ -49,7 +46,6 @@ network () {
     echo "arch" > /etc/hostname
 
     # install & enable network.
-    pacman -S networkmanager network-manager-applet --noconfirm
     systemctl enable NetworkManager
 
     # add entries for localhost to '/etc/hosts' file.
@@ -57,7 +53,6 @@ network () {
     echo -e 127.0.0.1'\t'localhost'\n'::1'\t\t'localhost'\n'127.0.1.1'\t'arch >> /etc/hosts
 
     # install & enable firewall.
-    pacman -S ufw --noconfirm
     systemctl enable ufw
 
     # allow outgoing & reject incoming.
@@ -69,102 +64,19 @@ network () {
 bluetooth () {
 
     # install bluetooth.
-    pacman -S blueman bluez bluez-utils --noconfirm
     lsmod | grep btusb
     rfkill unblock bluetooth
     systemctl enable bluetooth.service
 
 }
 
-audio () {
-
-    # install audio packages.
-    pacman -S pipewire lib32-pipewire wireplumber pipewire-audio pipewire-alsa pipewire-pulse sof-firmware pavucontrol alsa-utils --noconfirm
-
-}
-
-webcam () {
-    # install webcam packages.
-    pacman -S v4l-utils cameractrls --noconfirm
-
-}
-
-chipset () {
-
-    # install microcode for amd.
-
-    vendor="$(lscpu | grep 'Model name')"
-
-    if [[ "$vendor" == *"AMD"* ]]; then
-        echo "AMD CPU Found !"
-        pacman -S amd-ucode --noconfirm
-    fi
-
-}
-
-gpu () {
-
-    # DRI driver for 3D acceleration.
-    pacman -S mesa lib32-mesa --noconfirm
-
-    vendor="$( lspci -v | grep -iE 'vga|3d|2d')"
-
-    if [[ "$vendor" == *"AMD"* ]]; then
-        echo "AMD GPU Found !"
-
-        # DDX driver which provides 2D acceleration in Xorg
-        pacman -S xf86-video-amdgpu --noconfirm
-
-        # vulkan support
-        pacman -S vulkan-radeon lib32-vulkan-radeon --noconfirm
-
-        # accelerated video decoding
-        pacman -S libva-mesa-driver lib32-libva-mesa-driver --noconfirm
-
-        # "20-amdgpu.conf" will be copied in the config section.
-
-    fi
-
-}
-
 tui () {
-
-    # install packages for a seamless terminal workflow.
-    apps=(
-
-        'fish'                  # user-friendly shell
-        'fisher'                # fish package manager
-        'starship'              # shell prompt
-        'tldr'                  # concise command examples
-
-        'exa'                   # alternative to `ls`
-        'bat'                   # alternative to `cat`
-
-        'git'                   # version control
-        'github-cli'
-        'neovide'               # text editor
-        'xclip'                 # clipboard manipulation tool
-
-        'fd'                    # file search
-        'ripgrep'               # search tool that combines the usability of ag with the raw speed of grep
-
-        'nodejs'                # Evented I/O for V8 javascript
-        'npm'                   # package manager for javascript
-
-        'btop'                  # task manager
-        'ncdu'                  # disk util info
-
-    )
-
-    for app in "${apps[@]}"; do
-        pacman -S "$app" --noconfirm --needed
-    done
 
     # set preset for starship prompt
     starship preset nerd-font-symbols -o ~/.config/starship.toml
 
     # clone suckless fork. (this command also creates .config dir as root)
-    git clone https://github.com/commitsovercoffee/suckless.git /home/hope/.config/suckless
+    retry_command git clone https://github.com/commitsovercoffee/suckless.git /home/hope/.config/suckless
 
     # install suckless terminal
     cd  /home/hope/.config/suckless/st
@@ -187,44 +99,6 @@ tui () {
 }
 
 gui () {
-
-    apps=(
-
-        # install display server :
-
-        'xorg-server'               # xorg display server.
-        'xorg-xinit'                # xinit ~ to start xorg server.
-        'xorg-xrandr'               # tui for RandR extension.
-        'xorg-xclipboard'           # xclipboard ~ clipboard manager.
-        'xorg-xclipboard'           # xclipboard ~ clipboard manager.
-
-        # install graphical utils :
-
-        'picom'                     # X compositor.
-        'dunst'                     # notification daemon.
-        'xbindkeys' 	            # bind commands to certain keys.
-        'libnotify'                 # lib to send desktop notifications.
-        'brightnessctl' 	    # control brightness.
-        'xautolock' 	            # autolocker.
-        'cbatticon'                 # battery for systray.
-
-        'feh'                       # desktop wallpaper.
-        'gnome-themes-extra'        # window themes.
-        'papirus-icon-theme'        # icon themes.
-
-        'dmenu'                     # app menu.
-        'xfce4-appfinder' 	    # alt app menu.
-        'lxappearance-gtk3'         # theme switcher.
-        'lxinput-gtk3'              # configure keyboard & mouse.
-
-        'pcmanfm-gtk3'              # file manager.
-        'firefox'                   # browser.
-
-    )
-
-    for app in "${apps[@]}"; do
-        pacman -S "$app" --noconfirm --needed
-    done
 
     # install dynamic window manager.
     cd /home/hope/.config/suckless/dwm
@@ -259,31 +133,6 @@ users () {
 
     # enable sudo for wheel group.
     sed -i 's/# %wheel ALL=(ALL:ALL) ALL/ %wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
-
-    # create directories for user.
-    pacman -S xdg-user-dirs --noconfirm; xdg-user-dirs-update
-
-}
-
-grub () {
-
-    # install required packages.
-    pacman -S grub efibootmgr --noconfirm
-
-    # create directory to mount EFI partition.
-    mkdir /boot/efi
-
-    # mount the EFI partition.
-    mount /dev/nvme0n1p1 /boot/efi
-
-    # install grub.
-    grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot/efi
-
-    # enable logs.
-    sed -i 's/loglevel=3 quiet/loglevel=3/' /etc/default/grub
-
-    # generate grub config.
-    grub-mkconfig -o /boot/grub/grub.cfg
 
 }
 
@@ -329,7 +178,7 @@ config () {
 
     # 'neovim'
     mkdir -p /home/hope/.config/nvim
-    git clone --depth 1 https://github.com/commitsovercoffee/minima-nvim /home/hope/.config/suckless
+    retry_command git clone --depth 1 https://github.com/commitsovercoffee/minima-nvim /home/hope/.config/suckless
 
     # 'touchpad'
     mv .config/30-touch.conf /etc/X11/xorg.conf.d/30-touch.conf
@@ -337,6 +186,25 @@ config () {
     # reset permissions.
     chown -R  hope /home/hope/
     chown -R :hope /home/hope/
+
+}
+
+grub () {
+
+    # create directory to mount EFI partition.
+    mkdir /boot/efi
+
+    # mount the EFI partition.
+    mount /dev/nvme0n1p1 /boot/efi
+
+    # install grub.
+    grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot/efi
+
+    # enable logs.
+    sed -i 's/loglevel=3 quiet/loglevel=3/' /etc/default/grub
+
+    # generate grub config.
+    grub-mkconfig -o /boot/grub/grub.cfg
 
 }
 
@@ -348,87 +216,286 @@ misc() {
     # enable TRIM for SSDs.
     systemctl enable fstrim.timer
 
-    # encryption keys
-    pacman -S seahorse --noconfirm
-
-    # bug fix ~ reinstall pambase.
-    pacman -S pambase --noconfirm
-
-    # install apps
-    apps=(
-
-        'gnome-screenshot'      # screenshot tool.
-        'gcolor3'               # color picker.
-
-        'pcmanfm-gtk3'          # file manager.
-        'unzip'                 # extract/view .zip archives.
-        'file-roller' 		# create/modify archives.
-        'mtpfs'                 # read/write to MTP devices.
-        'libmtp'                # MTP support.
-        'gvfs'                  # gnome virtual file system for mounting.
-        'gvfs-mtp'              # gnome virtual file system for MTP devices.
-        'android-tools'         # android platform tools.
-        'android-udev'          # udev rules to connect to android.
-
-        'firefox'                   # primary browser.
-        'torbrowser-launcher'       # tertiary browser.
-        'vivaldi' 		    # secondary browser.
-
-        'gedit'                 # text editor.
-        'evince'                # doc viewer.
-        'ristretto'             # image viewer.
-        'xournalpp' 		# note taking + pdf annotation.
-
-        'inkscape'              # vector graphics editor.
-        'mypaint'               # (raster) painting app.
-        'kolourpaint' 		# paint program.
-        'obs-studio'            # screen cast/record.
-        'peek'     		# gif recorder.
-        'pitivi' 		# video editor.
-
-        'qbittorent'            # torrent client.
-        'gnome-disk-utility'    # disk management.
-
-        'mpv'                   # media player.
-        'handlr' 		# sets default apps.
-
-    )
-
-    for app in "${apps[@]}"; do
-        pacman -S "$app" --noconfirm
-    done
-
     # set default apps
-
     handlr set 'text/*' neovide.desktop
     handlr set 'audio/*' mpv.desktop
     handlr set 'image/*' org.xfce.ristretto.desktop
     handlr set 'application/pdf' org.gnome.Evince.desktop
 
+    # create directories for user.
+    xdg-user-dirs; xdg-user-dirs-update
+
 }
+
+# List of packages to download and install
+packages=(
+
+    # DRIVERS -----------------------------------------------------------------
+
+    # microcode
+    'amd-ucode'
+
+    # DRI driver for 3D acceleration.
+    'mesa'
+    'lib32-mesa'
+
+    # DDX driver which provides 2D acceleration in Xorg
+    'xf86-video-amdgpu'
+
+    # vulkan support
+    'vulkan-radeon'
+    'lib32-vulkan-radeon'
+
+    # accelerated video decoding
+    'libva-mesa-driver'
+    'lib32-libva-mesa-driver'
+
+    # audio
+    'pipewire'
+    'lib32-pipewire'
+    'wireplumber'
+    'pipewire-audio'
+    'pipewire-alsa'
+    'pipewire-pulse'
+    'sof-firmware'
+    'pavucontrol'
+    'alsa-utils'
+
+    # network & firewall
+    'networkmanager'
+    'network-manager-applet'
+    'ufw'
+
+    # bluetooth
+    'blueman'
+    'bluez'
+    'bluez-utils'
+
+    # webcam
+    'v4l-utils'
+    'cameractrls'
+
+
+    # TUI ---------------------------------------------------------------------
+
+    'fish'                  # user-friendly shell
+    'fisher'                # fish package manager
+    'starship'              # shell prompt
+    'tldr'                  # concise command examples
+    'cowsay' 		    # talking cow
+
+    'exa'                   # alternative to `ls`
+    'bat'                   # alternative to `cat`
+
+    'git'                   # version control
+    'github-cli'
+    'neovide'               # text editor
+    'xclip'                 # clipboard manipulation tool
+
+    'fd'                    # file search
+    'ripgrep'               # search tool
+
+    'nodejs'                # Evented I/O for V8 javascript
+    'npm'                   # package manager for javascript
+
+    'btop'                  # task manager
+    'ncdu'                  # disk util info
+
+    # GUI ---------------------------------------------------------------------
+
+    'xorg-server'               # xorg display server.
+    'xorg-xinit'                # xinit ~ to start xorg server.
+    'xorg-xrandr'               # tui for RandR extension.
+    'xorg-xclipboard'           # xclipboard ~ clipboard manager.
+    'xorg-xclipboard'           # xclipboard ~ clipboard manager.
+
+    'picom'                     # X compositor.
+    'dunst'                     # notification daemon.
+    'xbindkeys' 	        # bind commands to certain keys.
+    'libnotify'                 # lib to send desktop notifications.
+    'brightnessctl' 	    	# control brightness.
+    'xautolock' 	        # autolocker.
+    'cbatticon'                 # battery for systray.
+
+    'feh'                       # desktop wallpaper.
+    'gnome-themes-extra'        # window themes.
+    'papirus-icon-theme'        # icon themes.
+
+    'dmenu'                     # app menu.
+    'xfce4-appfinder' 	    	# alt app menu.
+    'lxappearance-gtk3'         # theme switcher.
+    'lxinput-gtk3'              # configure keyboard & mouse.
+
+    'ttf-firacode-nerd'  	# fonts ...
+    'nerd-fonts'
+    'noto-fonts'
+    'noto-fonts-extra'
+    'noto-fonts-emoji'
+    'font-manager'
+
+    'seahorse'
+    'pambase'
+
+    # APPS --------------------------------------------------------------------
+
+    'gnome-screenshot'      # screenshot tool.
+    'gcolor3'               # color picker.
+
+    'pcmanfm-gtk3'          # file manager.
+    'unzip'                 # extract/view .zip archives.
+    'file-roller' 		# create/modify archives.
+    'mtpfs'                 # read/write to MTP devices.
+    'libmtp'                # MTP support.
+    'gvfs'                  # gnome virtual file system for mounting.
+    'gvfs-mtp'              # gnome virtual file system for MTP devices.
+    'android-tools'         # android platform tools.
+    'android-udev'          # udev rules to connect to android.
+
+    'firefox'               # primary browser.
+    'torbrowser-launcher'   # tertiary browser.
+    'vivaldi' 	 	    # secondary browser.
+
+    'gedit'                 # text editor.
+    'evince'                # doc viewer.
+    'ristretto'             # image viewer.
+    'xournalpp' 	    # note taking + pdf annotation.
+
+    'inkscape'              # vector graphics editor.
+    'mypaint'               # (raster) painting app.
+    'kolourpaint' 		# paint program.
+    'obs-studio'            # screen cast/record.
+    'peek'     		    # gif recorder.
+    'pitivi' 		    # video editor.
+
+    'qbittorent'            # torrent client.
+    'gnome-disk-utility'    # disk management.
+
+    'mpv'                   # media player.
+    'handlr' 		    # sets default apps.
+
+
+    # GRUB --------------------------------------------------------------------
+
+    'grub'
+    'efibootmgr'
+
+)
+
+# Function to download packages
+download_packages() {
+
+    local failed_packages=()
+    for package in "${packages[@]}"; do
+        echo "Downloading $package..."
+        pacman -Sw --noconfirm --needed "$package"
+        if [ $? -ne 0 ]; then
+            echo "Download failed for $package."
+            failed_packages+=("$package")
+        fi
+    done
+    echo "${failed_packages[@]}"
+
+}
+
+# Function to install downloaded packages
+install_packages() {
+
+    local failed_packages=()
+    echo "Installing downloaded packages..."
+    pacman -U --noconfirm *.pkg.tar.zst
+    if [ $? -ne 0 ]; then
+        echo "Installation failed."
+        failed_packages=("${packages[@]}")
+    fi
+    echo "${failed_packages[@]}"
+
+}
+
+retry_command() {
+    local max_attempts="${1:-3}"  # Default to 3 attempts if not specified
+    shift
+    local attempt=1
+    until "$@"; do
+        if [ $attempt -ge $max_attempts ]; then
+            echo "Max attempts reached. Exiting."
+            exit 1
+        fi
+        ((attempt++))
+        echo "Retry attempt $attempt..."
+        sleep 5  # Adjust sleep duration as needed
+    done
+}
+
+# Main function
+main() {
+
+    echo "Verifying and downloading packages..."
+    local failed_downloads
+    while true; do
+        failed_downloads=$(download_packages)
+        if [ -z "$failed_downloads" ]; then
+            break
+        fi
+        echo "Retrying download for failed packages..."
+        packages=("${failed_downloads[@]}")
+    done
+
+    local failed_installs
+    while true; do
+        failed_installs=$(install_packages)
+        if [ -z "$failed_installs" ]; then
+            break
+        fi
+        echo "Retrying installation for failed packages..."
+        packages=("${failed_installs[@]}")
+        failed_downloads=$(download_packages)
+        if [ -n "$failed_downloads" ]; then
+            echo "Retrying download for failed packages..."
+        else
+            echo "Failed to download packages. Exiting."
+            exit 1
+        fi
+    done
+
+    echo "Packages Installation completed."
+
+}
+
+# install packages ...
+main
 
 # mark pwd
 current_dir=$PWD
 
 # setup ...
 
+clear; cowsay "Refreshing package database & adding 32-bit apps."; sleep 5;
 multilib
+
+clear; cowsay "Setting up time. Tick-Toc Tick-Toc!"; sleep 5;
 timezone
+
+clear; cowsay "Setting up locale. Hola? Namaste?"; sleep 5;
 locale
-users
 
+clear; cowsay "Setting up network ..."; sleep 5;
 network
-bluetooth
-audio
-webcam
-chipset
-gpu
 
+clear; cowsay "Setting up bluetooth ..."; sleep 5;
+bluetooth
+
+clear; cowsay "Setting Up Terminal."; sleep 5;
 tui
+
+clear; cowsay "Setting Up GUI."; sleep 5;
 gui
 
-grub
+clear; cowsay "Creating user ..."; sleep 5;
+users
 config
+
+clear; cowsay "Grub it!"; sleep 5;
+grub
 misc
 
 # clean dir & exit.
