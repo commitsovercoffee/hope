108873a0ee860ac038587f97edbb8596ed53ee63
commitsovercoffee <commitsovercoffee@gmail.com>
2025-03-27 04:52:24 +0530

    updates fish aliases

--- .config/config.fish
+++ .config/config.fish
@@ -2,34 +2,35 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# General :
-alias cat=bat
-alias ls="exa -l"
-alias open="xdg-open"
-alias vim=nvim
-alias top=btop
-alias bye="sudo rsync -r ~/Obsidian ~/Zion/Blue/; shutdown -h now"
-function weather
-    curl "wttr.in/$argv?format=3"
-end
+# Nav :
 
+alias ls="exa -l"
+alias cat=bat
 alias mkdir='mkdir -pv'
 alias grep='grep --color=auto'
+alias open="xdg-open"
 
-# Pacman : 
+# Pacman :
+
 alias sync="sudo pacman -Syy"
 alias update="sudo pacman -Syu"
 alias info="sudo pacman -Ss"
 alias install="sudo pacman -S"
 alias remove="sudo pacman -Rns"
 
-# Git 
-alias stats="git status"
-alias add="git add ."
-alias commit="git commit -m"
-alias push="git push"
+# Custom :
 
-# start X at login
+alias vim=nvim
+alias top=btop
+alias npm=pnpm
+
+alias bye="sudo rsync -r ~/Obsidian ~/Zion/Blue/; shutdown -h now"
+
+function weather
+    curl "wttr.in/$argv?format=3"
+end
+
+# start X at login.
 if status is-login
     if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
         exec startx -- -keeptty
